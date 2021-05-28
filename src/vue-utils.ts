68515ac4dad5140e5678eb8d1dd53368cedb293c
Alexander Noack <alexander.noack@diva-e.com>
2021-05-28 11:43:32 +0200

    chore: add check to prevent recursion in vue-utils

--- src/vue-utils.ts
+++ src/vue-utils.ts
@@ -1,5 +1,12 @@
 export function callVueSuperMethod (vueComponent: any, methodName: string, parameters: Array<any> = []) {
-  return vueComponent.constructor.super.options.methods[methodName].apply(vueComponent, parameters)
+  const thisMethod = vueComponent.constructor.options.methods[methodName]
+  const superMethod = vueComponent.constructor.super.options.methods[methodName]
+  if (thisMethod && thisMethod === superMethod) {
+    console.warn('super method is equal to the instance method; returning empty object as fallback')
+    return {}
+  }
+
+  return superMethod.apply(vueComponent, parameters)
 }
 
 export function getVueSuperComputedProp (vueComponent: any, attrName: string) {
