707a8312e51548b1eec2bbae5585eb02e9304f8e
Alexander Noack <alexander.noack@diva-e.com>
2021-05-28 09:12:47 +0200

    style: fix whitespaces in ResponsiveGrid.spec

--- tests/unit/Components/ResponsiveGrid.spec.ts
+++ tests/unit/Components/ResponsiveGrid.spec.ts
@@ -14,48 +14,48 @@
  *  limitations under the License.
  */
 
-import { ComponentMapping } from '../../../src/ComponentMapping';
-import { AllowedComponents } from '../../../src/components/allowedcomponents/AllowedComponentsContainer';
-import { ResponsiveGrid } from '../../../src/components/ResponsiveGrid';
+import { ComponentMapping } from '../../../src/ComponentMapping'
+import { AllowedComponents } from '../../../src/components/allowedcomponents/AllowedComponentsContainer'
+import { ResponsiveGrid } from '../../../src/components/ResponsiveGrid'
 import 'reflect-metadata'
-import { mount } from "@vue/test-utils";
+import { mount } from '@vue/test-utils'
 import { Component, Prop, Mixins, Vue } from 'vue-property-decorator'
 
 describe('ResponsiveGrid ->', () => {
-    const CONTAINER_PLACEHOLDER_SELECTOR = '.new.section';
-    const CONTAINER_PLACEHOLDER_DATA_ATTRIBUTE_SELECTOR = '[data-cq-data-path="/container/*"]';
-    const ROOT_CLASS_NAME = 'root-class';
-    const ITEM_CLASS_NAME = 'item-class';
-    const CONTAINER_PATH = '/container';
-    const ITEM1_DATA_ATTRIBUTE_SELECTOR = '[data-cq-data-path="/container/component1"]';
-    const ITEM2_DATA_ATTRIBUTE_SELECTOR = '[data-cq-data-path="/container/component2"]';
-    const GRID_CLASS_NAMES = 'grid-class-names';
-    const COLUMN_1_CLASS_NAMES = 'column-class-names-1';
-    const COLUMN_2_CLASS_NAMES = 'column-class-names-2';
-    const PLACEHOLDER_CLASS_NAMES = 'aem-Grid-newComponent';
-    const COMPONENT_TYPE1 = 'components/c1';
-    const COMPONENT_TYPE2 = 'components/c2';
+  const CONTAINER_PLACEHOLDER_SELECTOR = '.new.section'
+  const CONTAINER_PLACEHOLDER_DATA_ATTRIBUTE_SELECTOR = '[data-cq-data-path="/container/*"]'
+  const ROOT_CLASS_NAME = 'root-class'
+  const ITEM_CLASS_NAME = 'item-class'
+  const CONTAINER_PATH = '/container'
+  const ITEM1_DATA_ATTRIBUTE_SELECTOR = '[data-cq-data-path="/container/component1"]'
+  const ITEM2_DATA_ATTRIBUTE_SELECTOR = '[data-cq-data-path="/container/component2"]'
+  const GRID_CLASS_NAMES = 'grid-class-names'
+  const COLUMN_1_CLASS_NAMES = 'column-class-names-1'
+  const COLUMN_2_CLASS_NAMES = 'column-class-names-2'
+  const PLACEHOLDER_CLASS_NAMES = 'aem-Grid-newComponent'
+  const COMPONENT_TYPE1 = 'components/c1'
+  const COMPONENT_TYPE2 = 'components/c2'
 
-    const ITEMS = {
-        'component1': {
-            ':type': COMPONENT_TYPE1,
-            'id': 'c1'
-        },
-        'component2': {
-            ':type': COMPONENT_TYPE2,
-            'id': 'c2'
-        }
-    };
+  const ITEMS = {
+    component1: {
+      ':type': COMPONENT_TYPE1,
+      id: 'c1'
+    },
+    component2: {
+      ':type': COMPONENT_TYPE2,
+      id: 'c2'
+    }
+  }
 
-    const ITEMS_ORDER = [ 'component1', 'component2' ];
+  const ITEMS_ORDER = ['component1', 'component2']
 
-    const COLUMN_CLASS_NAMES = {
-        'component1': COLUMN_1_CLASS_NAMES,
-        'component2': COLUMN_2_CLASS_NAMES
-    };
+  const COLUMN_CLASS_NAMES = {
+    component1: COLUMN_1_CLASS_NAMES,
+    component2: COLUMN_2_CLASS_NAMES
+  }
 
     @Component
-    class DummyProps extends Vue{
+  class DummyProps extends Vue {
         @Prop() id!: string;
         @Prop({ default: false }) cqForceReload?: boolean;
         @Prop({ default: false }) isInEditor!: boolean;
@@ -64,110 +64,105 @@ describe('ResponsiveGrid ->', () => {
 
     @Component
     class ComponentChild extends Mixins(DummyProps) {
-        render(createElement: Function) {
-            return createElement('div', {
-                attrs: {
-                    id: this.id,
-                    class: ITEM_CLASS_NAME
-                }
-            })
-        }
+      render (createElement: Function) {
+        return createElement('div', {
+          attrs: {
+            id: this.id,
+            class: ITEM_CLASS_NAME
+          }
+        })
+      }
     }
 
     const allowedComp: AllowedComponents = {
-        applicable: false,
-        components: []
-    };
+      applicable: false,
+      components: []
+    }
 
     const STANDARD_GRID_PROPS = {
-        cqPath: '',
-        gridClassNames: '',
-        columnClassNames: {},
-        allowedComponents: allowedComp,
-        title: '',
-        componentMapping: ComponentMapping,
-        cqItems: {},
-        cqItemsOrder: [],
-        isInEditor: false
-    };
+      cqPath: '',
+      gridClassNames: '',
+      columnClassNames: {},
+      allowedComponents: allowedComp,
+      title: '',
+      componentMapping: ComponentMapping,
+      cqItems: {},
+      cqItemsOrder: [],
+      isInEditor: false
+    }
 
-    let rootNode: any;
-    let ComponentMappingSpy: jest.SpyInstance;
+    let rootNode: any
+    let ComponentMappingSpy: jest.SpyInstance
 
     beforeEach(() => {
-        ComponentMappingSpy = jest.spyOn(ComponentMapping, 'get');
-        rootNode = document.createElement('div');
-        rootNode.className = ROOT_CLASS_NAME;
-        document.body.appendChild(rootNode);
-    });
+      ComponentMappingSpy = jest.spyOn(ComponentMapping, 'get')
+      rootNode = document.createElement('div')
+      rootNode.className = ROOT_CLASS_NAME
+      document.body.appendChild(rootNode)
+    })
 
     afterEach(() => {
-        ComponentMappingSpy.mockRestore();
+      ComponentMappingSpy.mockRestore()
 
-        if (rootNode) {
-            document.body.appendChild(rootNode);
-            rootNode = undefined;
-        }
-    });
+      if (rootNode) {
+        document.body.appendChild(rootNode)
+        rootNode = undefined
+      }
+    })
 
     describe('Grid class names ->', () => {
-        it('should add the grid class names', () => {
+      it('should add the grid class names', () => {
+        const vm = mount(ResponsiveGrid, {
+          attachTo: rootNode,
+          propsData: {
+            ...STANDARD_GRID_PROPS
+          }
+        })
 
-            const vm = mount(ResponsiveGrid, {
-                attachTo: rootNode,
-                propsData: {
-                    ...STANDARD_GRID_PROPS
-                }
-            });
+        const gridElement = vm.find('.' + GRID_CLASS_NAMES)
 
-            const gridElement = vm.find('.' + GRID_CLASS_NAMES);
-
-            expect(gridElement).toBeDefined();
-        });
-
-    });
+        expect(gridElement).toBeDefined()
+      })
+    })
 
     describe('Placeholder ->', () => {
-        it('should add the expected placeholder class names', () => {
+      it('should add the expected placeholder class names', () => {
+        const vm = mount(ResponsiveGrid, {
+          attachTo: rootNode,
+          propsData: {
+            ...STANDARD_GRID_PROPS,
+            isInEditor: true,
+            cqPath: CONTAINER_PATH
+          }
+        })
 
-            const vm = mount(ResponsiveGrid, {
-                attachTo: rootNode,
-                propsData: {
-                    ...STANDARD_GRID_PROPS,
-                    isInEditor: true,
-                    cqPath: CONTAINER_PATH
-                }
-            });
+        const gridPlaceholder = vm.find('.' + PLACEHOLDER_CLASS_NAMES + CONTAINER_PLACEHOLDER_SELECTOR + CONTAINER_PLACEHOLDER_DATA_ATTRIBUTE_SELECTOR)
 
-
-            const gridPlaceholder = vm.find('.' + PLACEHOLDER_CLASS_NAMES + CONTAINER_PLACEHOLDER_SELECTOR + CONTAINER_PLACEHOLDER_DATA_ATTRIBUTE_SELECTOR);
-
-            expect(gridPlaceholder).toBeDefined();
-        });
-    });
+        expect(gridPlaceholder).toBeDefined()
+      })
+    })
 
     describe('Column class names ->', () => {
-        it('should add the expected column class names', () => {
-            ComponentMappingSpy.mockReturnValue(ComponentChild);
+      it('should add the expected column class names', () => {
+        ComponentMappingSpy.mockReturnValue(ComponentChild)
 
-            const vm = mount(ResponsiveGrid, {
-                attachTo: rootNode,
-                propsData: {
-                    ...STANDARD_GRID_PROPS,
-                    columnClassNames: COLUMN_CLASS_NAMES,
-                    cqItems: ITEMS,
-                    cqItemsOrder: ITEMS_ORDER,
-                    isInEditor: true,
-                    cqPath: CONTAINER_PATH
-                }
-            });
+        const vm = mount(ResponsiveGrid, {
+          attachTo: rootNode,
+          propsData: {
+            ...STANDARD_GRID_PROPS,
+            columnClassNames: COLUMN_CLASS_NAMES,
+            cqItems: ITEMS,
+            cqItemsOrder: ITEMS_ORDER,
+            isInEditor: true,
+            cqPath: CONTAINER_PATH
+          }
+        })
 
+        const childItem1 = vm.find('.' + COLUMN_1_CLASS_NAMES + ITEM1_DATA_ATTRIBUTE_SELECTOR)
+        const childItem2 = vm.find('.' + COLUMN_2_CLASS_NAMES + ITEM2_DATA_ATTRIBUTE_SELECTOR)
 
-            const childItem1 = vm.find('.' + COLUMN_1_CLASS_NAMES + ITEM1_DATA_ATTRIBUTE_SELECTOR);
-            const childItem2 = vm.find('.' + COLUMN_2_CLASS_NAMES + ITEM2_DATA_ATTRIBUTE_SELECTOR);
-
-            expect(childItem1).toBeDefined();
-            expect(childItem2).toBeDefined();
-        });
-    });
-});
+        expect(childItem1).toBeDefined()
+        expect(childItem2).toBeDefined()
+      })
+    })
+})
